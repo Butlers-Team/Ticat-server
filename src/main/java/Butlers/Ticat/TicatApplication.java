0030fe3810456bf8ed4005d2bede36935c6a6fb4
jy3026 <jy_3026@naver.com>
2023-06-09 16:47:05 +0900

    초기세팅

---
+++ src/main/java/Butlers/Ticat/TicatApplication.java
@@ -0,0 +1,13 @@
+package Butlers.Ticat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Butlers/Ticat/TicatApplicationTests.java
@@ -0,0 +1,13 @@
+package Butlers.Ticat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
