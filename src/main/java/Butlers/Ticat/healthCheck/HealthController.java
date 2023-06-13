daf513c307730e45b3c33fc2ad6b76dd99586476
RapiL <sjw3090gmail.com>
2023-06-13 17:07:42 +0900

    Feat: api 체크 컨트롤러

---
+++ src/main/java/Butlers/Ticat/healthCheck/HealthController.java
@@ -0,0 +1,16 @@
+package Butlers.Ticat.healthCheck;
+
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
+    }
+}
