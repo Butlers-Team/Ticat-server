f85bfffd0d0fb2016fcd3dcba42635620c2d2617
jy3026 <jy_3026@naver.com>
2023-06-27 21:12:48 +0900

    feat : festival 세부 내용을 가져오는 엔드포인트 추가

--- src/main/java/Butlers/Ticat/api/controller/FestivalApiController.java
+++ src/main/java/Butlers/Ticat/api/controller/FestivalApiController.java
@@ -17,16 +17,24 @@ public class FestivalApiController {
     private final FestivalApiService festivalApiService;
 
     @PostMapping("/list")
-    public ResponseEntity<String> resetTourList() throws IOException {
+    public ResponseEntity<String> resetFestivalList() throws IOException {
         festivalApiService.getFestivalList();
 
         return ResponseEntity.ok("행사리스트 불러오기");
     }
 
     @PostMapping("/detail")
-    public ResponseEntity<String> detailTourList() throws IOException {
+    public ResponseEntity<String> detailFestivalList() throws IOException {
         festivalApiService.getFestivalDetail();
 
-        return ResponseEntity.ok("행사상세 불러오기");
+        return ResponseEntity.ok("행사상세1 불러오기");
     }
+
+    @PostMapping("/overview")
+    public ResponseEntity<String> overviewFestivalList() throws IOException {
+        festivalApiService.getFestivalOverView();
+
+        return ResponseEntity.ok("행사상세2 불러오기");
+    }
+
 }
