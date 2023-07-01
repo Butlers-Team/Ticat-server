b108f1ceecee4aae0a56529f28b242e75dc88599
jy3026 <jy_3026@naver.com>
2023-07-01 17:17:35 +0900

    feat: 함수 ST_DISTANCE_SPHERE를 사용해 일정거리 안에 있는 축제를 출력하게 만드는 레포지토리 추가

--- src/main/java/Butlers/Ticat/festival/repository/FestivalRepository.java
+++ src/main/java/Butlers/Ticat/festival/repository/FestivalRepository.java
@@ -2,11 +2,17 @@ package Butlers.Ticat.festival.repository;
 
 import Butlers.Ticat.festival.entity.Festival;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FestivalRepository extends JpaRepository<Festival,Long> {
 
     Optional<Festival> findByContentId(String contentId);
 
+    // 두 지점 사이의 거리를 구하는 데 사용하는 함수 ST_DISTANCE_SPHERE
+    @Query("SELECT f FROM Festival f WHERE ST_DISTANCE_SPHERE(POINT(f.mapx, f.mapy), POINT(:longitude, :latitude)) <= :distance")
+    List<Festival> findFestivalsWithinDistance(@Param("latitude") double latitude, @Param("longitude") double longitude, @Param("distance") double distance);
 }
