2676f16fbe619548f04d231efd6ee4b6c97f8cde
jy3026 <jy_3026@naver.com>
2023-07-01 21:40:48 +0900

    feat: 축제 상세페이지 ResponseDto 추가

--- src/main/java/Butlers/Ticat/festival/dto/FestivalDto.java
+++ src/main/java/Butlers/Ticat/festival/dto/FestivalDto.java
@@ -8,12 +8,31 @@ public class FestivalDto {
     @Setter
     public static class ListResponse{
 
-        private String contentId;
+        private Long contentId;
         private String title;
         private String image;
         private String eventDate;
         private Double rating;
         private Integer like;
+        private double mapx;
+        private double mapy;
+    }
+
+    @Getter
+    @Setter
+    public static class Response{
+
+        private Long contentId;
+        private String eventDate;
+        private String image;
+        private String title;
+        private String address;
+        private String overview;
+        private String tel;
+        private String eventhomepage;
+        private String eventplace; // 이게 비어있다면 주소 그대로 노출
+        private String usetimefestival;
+        private String playtime;
         private double mapx;
         private double mapy;
     }
