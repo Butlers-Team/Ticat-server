9c99a5f2e6962f8ed79896f538169aedcba55bb8
jy3026 <jy_3026@naver.com>
2023-07-03 17:27:58 +0900

    feat: 네이버 블로그 검색 응답 데이터 추가

---
+++ src/main/java/Butlers/Ticat/blog/dto/NaverBlogDto.java
@@ -0,0 +1,21 @@
+package Butlers.Ticat.blog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+public class NaverBlogDto {
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Response {
+        private String title;
+        private String link;
+        private String description;
+
+        private String bloggername;
+
+        private String postdate;
+    }
+}
