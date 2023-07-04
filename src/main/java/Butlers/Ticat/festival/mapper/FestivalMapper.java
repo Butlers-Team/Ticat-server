991efb9deaf08d2904f22b07cfacbfd614005406
jy3026 <jy_3026@naver.com>
2023-07-04 17:35:10 +0900

    fix: 응답데이터 매핑데이터 추가

--- src/main/java/Butlers/Ticat/festival/mapper/FestivalMapper.java
+++ src/main/java/Butlers/Ticat/festival/mapper/FestivalMapper.java
@@ -11,16 +11,20 @@ import java.util.List;
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface FestivalMapper {
 
-    @Mapping(source = "detailFestival.eventDate", target = "eventDate")
+    @Mapping(source = "detailFestival.eventstartdate", target = "eventstartdate")
+    @Mapping(source = "detailFestival.eventenddate", target = "eventenddate")
     @Mapping(source = "detailFestival.overview", target = "overview")
     @Mapping(source = "detailFestival.eventhomepage", target = "eventhomepage")
     @Mapping(source = "detailFestival.eventplace", target = "eventplace")
-    @Mapping(source = "detailFestival.usetimefestival", target = "usetimefestival")
+    @Mapping(source = "detailFestival.usetimefestival", target = "price")
     @Mapping(source = "detailFestival.playtime", target = "playtime")
+    @Mapping(source = "detailFestival.status", target = "status")
     FestivalDto.Response festivalToResponse(Festival festival);
 
-    @Mapping(source = "detailFestival.eventDate", target = "eventDate")
+    @Mapping(source = "detailFestival.eventstartdate", target = "eventstartdate")
+    @Mapping(source = "detailFestival.eventenddate", target = "eventenddate")
     @Mapping(source = "detailFestival.category", target = "category")
+    @Mapping(source = "detailFestival.status", target = "status")
     FestivalDto.ListResponse festivalToListResponse(Festival festival);
     List<FestivalDto.ListResponse> festivalsToFestivalListResponses(List<Festival> festivals);
 }
