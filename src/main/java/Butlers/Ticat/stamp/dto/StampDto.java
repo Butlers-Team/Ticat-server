e675b68820e684e8909afefc3a06652f246169a7
RapiL <sjw3090gmail.com>
2023-07-05 12:46:01 +0900

    feat: StampDto 추가

---
+++ src/main/java/Butlers/Ticat/stamp/dto/StampDto.java
@@ -0,0 +1,33 @@
+package Butlers.Ticat.stamp.dto;
+
+import Butlers.Ticat.festival.dto.FestivalDto;
+import Butlers.Ticat.festival.entity.Festival;
+import Butlers.Ticat.stamp.entity.Stamp;
+import lombok.*;
+
+import java.util.List;
+
+public class StampDto {
+    @Getter
+    @Setter
+    public static class Post {
+        private Long memberId;
+        private Long contentId;
+    }
+
+
+    @Getter
+    @Setter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Response {
+        private Long memberId;
+
+
+        private List<FestivalDto.StampResponse> festivalList;
+//        private String eventStartDate;
+//        private String eventEndDate;
+
+    }
+}
