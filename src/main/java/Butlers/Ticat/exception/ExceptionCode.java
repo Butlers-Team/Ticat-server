40311cd86b1b2249cbc3d30bed3341677276fe8b
RapiL <sjw3090gmail.com>
2023-07-05 14:28:44 +0900

    fix: 내용 추가

--- src/main/java/Butlers/Ticat/exception/ExceptionCode.java
+++ src/main/java/Butlers/Ticat/exception/ExceptionCode.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 public enum ExceptionCode {
 
     MEMBER_NOT_FOUND(404, "회원을 찾을 수 없습니다"),
-    MEMBER_EMAIL_EXISTS(409, "이메일이 이미 있습니다" );
+    MEMBER_EMAIL_EXISTS(409, "이메일이 이미 있습니다" ),
+    FESTIVAL_NOT_FOUND(404, "축제를 찾을 수 없습니다"),
+    STAMP_NOT_FOUND(404, "스탬프를 찾을 수 없습니다");
 
     @Getter
     private int status;
