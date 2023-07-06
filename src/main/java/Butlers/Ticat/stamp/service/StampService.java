b2b9ff34024862ee3973c6fd28e01575a3ce35ff
RapiL <sjw3090gmail.com>
2023-07-06 13:55:59 +0900

    fix: 스템프 찍는 날짜 추가

--- src/main/java/Butlers/Ticat/stamp/service/StampService.java
+++ src/main/java/Butlers/Ticat/stamp/service/StampService.java
@@ -2,23 +2,15 @@ package Butlers.Ticat.stamp.service;
 
 import Butlers.Ticat.exception.BusinessLogicException;
 import Butlers.Ticat.exception.ExceptionCode;
-import Butlers.Ticat.festival.entity.DetailFestival;
 import Butlers.Ticat.festival.entity.Festival;
 import Butlers.Ticat.festival.repository.FestivalRepository;
 import Butlers.Ticat.member.entity.Member;
 import Butlers.Ticat.member.repository.MemberRepository;
-import Butlers.Ticat.stamp.dto.StampDto;
-import Butlers.Ticat.stamp.entity.Location;
 import Butlers.Ticat.stamp.entity.Stamp;
 import Butlers.Ticat.stamp.repository.StampRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import java.time.YearMonth;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.time.LocalDate;
 
 @Service
 public class StampService {
@@ -34,38 +26,6 @@ public class StampService {
     }
 
 
-    public List<Festival> getNearbyFestivals(Location clientLocation) {
-        List<Festival> allFestivals = festivalRepository.findAll();
-        List<Festival> nearbyFestivals = new ArrayList<>();
-
-        for (Festival festival : allFestivals) {
-            if (isWithin500m(clientLocation, festival.getLocation())) {
-                nearbyFestivals.add(festival);
-            }
-        }
-
-        return nearbyFestivals;
-    }
-
-    private boolean isWithin500m(Location location1, Location location2) {
-
-        double distance = location1.distanceTo(location2);
-        return distance <= 500; // 거리
-    }
-
-    public void stampFestivalsWithin500m(Location memberLocation, Member member) {
-        List<Festival> nearbyFestivals = getNearbyFestivals(memberLocation);
-
-        for (Festival festival : nearbyFestivals) {
-            Stamp stamp = Stamp.builder()
-                    .festival(festival)
-                    .member(member)
-                    .build();
-
-            stampRepository.save(stamp);
-        }
-    }
-
     public Stamp saveStamp(Long memberId, Long contentId) {
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
@@ -77,32 +37,8 @@ public class StampService {
 
         stamp.setMember(member);
         stamp.setFestival(festival);
-
-        DetailFestival detailFestival = festival.getDetailFestival();
-        if (detailFestival != null) {
-
-            stamp.setTitle(festival.getTitle());
-            stamp.setAddress(festival.getAddress());
-            stamp.setEventDate(detailFestival.getEventDate());
-        }
+        stamp.setStampDate(LocalDate.now());
 
         return stampRepository.save(stamp);
-    }
-
-    public Stamp getStamp(Long memberId) {
-        Stamp stamp = stampRepository.findById(memberId)
-                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.STAMP_NOT_FOUND));
-        return stamp;
-    }
-
-    public Page<Festival> findFestivals(int page, int size, Integer year, Integer month) {
-        PageRequest pageRequest = PageRequest.of(page - 1, size);
-
-        if (year != null && month != null) {
-            YearMonth searchYearMonth = YearMonth.of(year, month);
-            return festivalRepository.findByYearAndMonth(searchYearMonth, pageRequest);
-        } else {
-            return festivalRepository.findAll(pageRequest);
-        }
     }
 }
