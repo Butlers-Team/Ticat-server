fc0111a0737c90abf9a88e2c6a6141546eb4f968
RapiL <sjw3090gmail.com>
2023-07-06 14:04:08 +0900

    feat: 회원의 스탬프 응답데이터 로직 추가

--- src/main/java/Butlers/Ticat/member/mapper/MemberMapper.java
+++ src/main/java/Butlers/Ticat/member/mapper/MemberMapper.java
@@ -1,9 +1,12 @@
 package Butlers.Ticat.member.mapper;
 
+import Butlers.Ticat.festival.dto.FestivalDto;
 import Butlers.Ticat.member.dto.MemberDto;
 import Butlers.Ticat.member.entity.Member;
+import Butlers.Ticat.stamp.entity.Stamp;
 import org.mapstruct.Mapper;
-import org.springframework.http.HttpStatus;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
@@ -14,4 +17,18 @@ public interface MemberMapper {
     MemberDto.Response memberToMemberResponse(Member member);
 
     Member memberPatchToMember(MemberDto.Patch requestBody);
+
+    default List<FestivalDto.StampResponse> getResponses(List<Stamp> stamps) {
+        return stamps.stream()
+                .map(stamp -> FestivalDto.StampResponse.builder()
+                        .contentId(stamp.getFestival().getContentId())
+                        .status(stamp.getFestival().getDetailFestival().getStatus())
+                        .stampDate(stamp.getStampDate())
+                        .title(stamp.getFestival().getTitle())
+                        .address(stamp.getFestival().getAddress())
+                        .eventStartDate(stamp.getFestival().getDetailFestival().getEventstartdate())
+                        .eventEndDate(stamp.getFestival().getDetailFestival().getEventenddate())
+                        .build())
+                .collect(Collectors.toList());
+    }
 }
