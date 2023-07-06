9f6c4c8ab0d54c8c407dac870335114616bf2702
RapiL <sjw3090gmail.com>
2023-07-06 14:01:04 +0900

    fix: 스탬프 날짜 필드 추가

--- src/main/java/Butlers/Ticat/stamp/entity/Stamp.java
+++ src/main/java/Butlers/Ticat/stamp/entity/Stamp.java
@@ -1,11 +1,11 @@
 package Butlers.Ticat.stamp.entity;
 
-import Butlers.Ticat.festival.entity.DetailFestival;
 import Butlers.Ticat.festival.entity.Festival;
 import Butlers.Ticat.member.entity.Member;
 import lombok.*;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -18,7 +18,7 @@ public class Stamp {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long StampId;
-
+    private LocalDate stampDate;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "festival_id")
     private Festival festival;
@@ -27,10 +27,4 @@ public class Stamp {
     @JoinColumn(name = "member_id")
     private Member member;
 
-
-//    private DetailFestival detailFestival;
-
-    private String title;
-    private String address;
-    private String eventDate;
 }
