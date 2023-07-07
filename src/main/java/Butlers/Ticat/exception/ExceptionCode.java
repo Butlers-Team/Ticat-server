636de4c54b8a531dcb3705fe59f0626c5b4031f0
jy3026 <jy_3026@naver.com>
2023-07-08 00:05:25 +0900

    feat: 좋아요 에러코드 추가

--- src/main/java/Butlers/Ticat/exception/ExceptionCode.java
+++ src/main/java/Butlers/Ticat/exception/ExceptionCode.java
@@ -5,8 +5,10 @@ import lombok.Getter;
 public enum ExceptionCode {
 
     MEMBER_NOT_FOUND(404, "회원을 찾을 수 없습니다"),
-    MEMBER_EMAIL_EXISTS(409, "이메일이 이미 있습니다" );
+    MEMBER_EMAIL_EXISTS(409, "이메일이 이미 있습니다" ),
+    LIKE_NOT_TWICE(409,"좋아요를 두 번 할 수 없습니다"),
 
+    LIKE_NOT_CANCEL(204,"좋아요를 취소할 수 없습니다");
     @Getter
     private int status;
     @Getter
