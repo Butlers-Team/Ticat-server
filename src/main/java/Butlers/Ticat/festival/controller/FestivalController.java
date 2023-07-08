01194d589fb5dd9181f84d8522545ac083baa130
jy3026 <jy_3026@naver.com>
2023-07-09 01:31:58 +0900

    feat: 지도페이지 엔드포인트 구현

--- src/main/java/Butlers/Ticat/festival/controller/FestivalController.java
+++ src/main/java/Butlers/Ticat/festival/controller/FestivalController.java
@@ -39,11 +39,11 @@ public class FestivalController {
 //    }
 
     // 축제 상세 페이지
-    @GetMapping("/{festivalId}")
-    public ResponseEntity getFestival(@Positive @PathVariable("festivalId") long festivalId){
+    @GetMapping("/{festival-id}")
+    public ResponseEntity getFestival(@Positive @PathVariable("festival-id") long festivalId){
         Festival festival = festivalService.findFestival(festivalId);
 
-        return new ResponseEntity<>(new SingleResponseDto<>(mapper.festivalToResponse(festival)),HttpStatus.OK);
+        return new ResponseEntity<>(mapper.festivalToResponse(festival),HttpStatus.OK);
     }
 
     @GetMapping("/distance")
@@ -97,6 +97,58 @@ public class FestivalController {
         }
     }
 
+    // 지도페이지
+    @GetMapping("/map")
+    public ResponseEntity getFilteredFestivalsByMap(@RequestParam(required = false) List<String> categories,
+                                                    @RequestParam(required = false) String sortBy,
+                                                    @Positive @RequestParam int page,
+                                                    @Positive @RequestParam int size) {
+        Page<Festival> pageFestivals;
+        List<Festival> festivals;
+
+        if (categories != null && !categories.isEmpty()) {
+            if (sortBy != null) {
+                switch (sortBy) {
+                    case "likeCount":
+                        pageFestivals = festivalService.findByCategoriesAndSortByLikeCount(categories, page, size);
+                        break;
+                    case "reviewRating":
+                        pageFestivals = festivalService.findByCategoriesAndSortByReviewRating(categories, page, size);
+                        break;
+                    case "reviewCount":
+                        pageFestivals = festivalService.findByCategoriesAndSortByReviewCount(categories, page, size);
+                        break;
+                    default:
+                        pageFestivals = festivalService.findByCategories(categories, page, size);
+                        break;
+                }
+            } else {
+                pageFestivals = festivalService.findByCategories(categories, page, size);
+            }
+        } else {
+            if (sortBy != null) {
+                switch (sortBy) {
+                    case "likeCount":
+                        pageFestivals = festivalService.findFestivalsByLikeCount(page, size);
+                        break;
+                    case "reviewRating":
+                        pageFestivals = festivalService.findFestivalsByReviewRating(page, size);
+                        break;
+                    case "reviewCount":
+                        pageFestivals = festivalService.findFestivalsByReviewCount(page, size);
+                        break;
+                    default:
+                        pageFestivals = festivalService.findFestivals(page, size);
+                        break;
+                }
+            } else {
+                pageFestivals = festivalService.findFestivals(page, size);
+            }
+        }
+        festivals = pageFestivals.getContent();
+        return new ResponseEntity<>(new MultiResponseDto<>(mapper.festivalsToFestivalListResponses(festivals), pageFestivals), HttpStatus.OK);
+    }
+
     @PostMapping("/{festival-id}/favorite")
     public ResponseEntity<String> postFavorite(@PathVariable("festival-id") @Positive long festivalId) {
         festivalService.createFavorite(festivalId);
@@ -112,5 +164,5 @@ public class FestivalController {
     }
 
 
-
 }
+
