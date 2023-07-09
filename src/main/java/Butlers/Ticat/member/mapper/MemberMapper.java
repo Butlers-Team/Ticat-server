1c0fc629ae1fa96cac45169eeca28cf93fa75f64
RapiL <sjw3090gmail.com>
2023-07-09 12:33:41 +0900

    feat: 회원의 캘린더 응답데이터 추가

--- src/main/java/Butlers/Ticat/member/mapper/MemberMapper.java
+++ src/main/java/Butlers/Ticat/member/mapper/MemberMapper.java
@@ -1,9 +1,13 @@
 package Butlers.Ticat.member.mapper;
 
+import Butlers.Ticat.calendar.dto.CalendarDto;
+import Butlers.Ticat.calendar.entity.Calendar;
 import Butlers.Ticat.member.dto.MemberDto;
 import Butlers.Ticat.member.entity.Member;
 import org.mapstruct.Mapper;
-import org.springframework.http.HttpStatus;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
@@ -14,4 +18,19 @@ public interface MemberMapper {
     MemberDto.Response memberToMemberResponse(Member member);
 
     Member memberPatchToMember(MemberDto.Patch requestBody);
+
+    default List<CalendarDto.CalendarResponse> getResponses(List<Calendar> calendars) {
+        return calendars.stream()
+                .map(calendar -> CalendarDto.CalendarResponse.builder()
+                        .contentId(calendar.getFestival().getContentId())
+                        .staus(calendar.getFestival().getDetailFestival().getStatus())
+                        .calendarDate(calendar.getCalendarDate())
+                        .title(calendar.getFestival().getTitle())
+                        .address(calendar.getFestival().getAddress())
+                        .eventStartDate(calendar.getFestival().getDetailFestival().getEventstartdate())
+                        .eventEndDate(calendar.getFestival().getDetailFestival().getEventenddate())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
 }
