dad3b4ff13735d655a7b670b65085618a22ae95e
RapiL <sjw3090gmail.com>
2023-07-09 12:28:50 +0900

    fix: Dto 수정 - 잘못된 엔티티 입력으로 수정

--- src/main/java/Butlers/Ticat/calendar/dto/CalendarDto.java
+++ src/main/java/Butlers/Ticat/calendar/dto/CalendarDto.java
@@ -1,8 +1,9 @@
 package Butlers.Ticat.calendar.dto;
 
-import Butlers.Ticat.festival.dto.FestivalDto;
+import Butlers.Ticat.festival.entity.DetailFestival;
 import lombok.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class CalendarDto {
@@ -14,6 +15,21 @@ public class CalendarDto {
     @AllArgsConstructor
     public static class Response {
         private Long memberId;
-        private List<FestivalDto.CalendarResponse> festivalList;
+        private List<CalendarResponse> festivalList;
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    public static class CalendarResponse{
+
+        private Long contentId;
+        private String category;
+        private DetailFestival.Status staus;
+        private LocalDate calendarDate;
+        private String eventStartDate; // 행사시작일
+        private String eventEndDate; // 행사 종료일
+        private String title;
+        private String address;
     }
 }
