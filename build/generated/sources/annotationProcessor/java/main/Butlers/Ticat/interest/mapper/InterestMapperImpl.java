5913d89e5338ca1debe6cde97e87ff5c33cc4e7e
jy3026 <jy_3026@naver.com>
2023-07-19 00:55:07 +0900

    fix: 리뷰 등록,수정,삭제시 축제의 리뷰 평점,개수 수정 로직 구현

---
+++ build/generated/sources/annotationProcessor/java/main/Butlers/Ticat/interest/mapper/InterestMapperImpl.java
@@ -0,0 +1,13 @@
+package Butlers.Ticat.interest.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-07-19T00:25:53+0900",
+    comments = "version: 1.5.1.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.6.1.jar, environment: Java 15.0.2 (Oracle Corporation)"
+)
+@Component
+public class InterestMapperImpl implements InterestMapper {
+}
