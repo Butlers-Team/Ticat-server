0516b8f4c7101892cfc05751b3efb7d0319e0d9b
jy3026 <jy_3026@naver.com>
2023-07-19 23:08:48 +0900

    feat: 스케줄러를 활용하여 매일 정각 축제 데이터 최신화

--- src/main/java/Butlers/Ticat/festival/service/FestivalApiService.java
+++ src/main/java/Butlers/Ticat/festival/service/FestivalApiService.java
@@ -1,6 +1,5 @@
 package Butlers.Ticat.festival.service;
 
-import Butlers.Ticat.festival.dto.FestivalDto;
 import Butlers.Ticat.festival.entity.DetailFestival;
 import Butlers.Ticat.festival.entity.Festival;
 import Butlers.Ticat.festival.repository.FestivalRepository;
@@ -9,10 +8,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Sort;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -28,7 +27,6 @@ import static Butlers.Ticat.festival.helper.KeywordCategoryMapping.determineCate
 import static Butlers.Ticat.festival.helper.KeywordCategoryMapping.keywordCategoryMap;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class FestivalApiService {
 
@@ -37,10 +35,11 @@ public class FestivalApiService {
 
     private final FestivalRepository festivalRepository;
 
+    @Scheduled(cron = "0 0 0 * * *") // 자정 설정
     public void getFestivalList() throws IOException {
         StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B551011/KorService1/areaBasedList1");
         urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + serviceKey);
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("100", "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("10", "UTF-8"));
         urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8"));
         urlBuilder.append("&" + URLEncoder.encode("MobileOS", "UTF-8") + "=" + URLEncoder.encode("ETC", "UTF-8"));
         urlBuilder.append("&" + URLEncoder.encode("MobileApp", "UTF-8") + "=" + URLEncoder.encode("AppTest", "UTF-8"));
@@ -100,10 +99,12 @@ public class FestivalApiService {
 
     public void getFestivalDetail() throws IOException {
 
-        List<Festival> festivalList = festivalRepository.findAll();
+        List<Festival> festivalList = festivalRepository.findAll(Sort.by(Sort.Direction.DESC, "festivalId"));
 
 
         for(Festival festival : festivalList){
+
+            if(festival.getDetailFestival().getEventstartdate() != null) continue;
 
             StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B551011/KorService1/detailIntro1");
             urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + serviceKey);
@@ -168,10 +169,12 @@ public class FestivalApiService {
 
     public void getFestivalOverView() throws IOException {
 
-        List<Festival> festivalList = festivalRepository.findAll();
+        List<Festival> festivalList = festivalRepository.findAll(Sort.by(Sort.Direction.DESC, "festivalId"));
 
 
         for(Festival festival : festivalList){
+
+            if(festival.getDetailFestival().getOverview() != null) continue;
 
             StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B551011/KorService1/detailCommon1");
             urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + serviceKey);
@@ -215,10 +218,96 @@ public class FestivalApiService {
                 String overview = obj.optString("overview", ""); // null일 경우 ""로 반환됨
                 String category = determineCategory(overview, keywordCategoryMap);
 
+                if(festival.getDetailFestival().getEventstartdate() != null &&  festival.getDetailFestival().getEventenddate() != null){
+                    festival.getDetailFestival().updateStatus();
+                }
 
                 festival.getDetailFestival().updateCategoryAndOverView(category, overview);
+
                 festivalRepository.save(festival);
 
+            }catch (Exception e) {
+                System.err.println("Failed to parse JSON data: " + e.getMessage());
+                continue;
+            }
+        }
+    }
+    @Scheduled(cron = "0 3 0 * * *") // 매일 0시 3분 실행
+    public void checkStatus(){
+        List<Festival> festivalList = festivalRepository.findAll();
+
+        for(Festival festival : festivalList){
+            if(festival.getDetailFestival().getEventstartdate() != null &&  festival.getDetailFestival().getEventenddate() != null){
+                festival.getDetailFestival().updateStatus();
+                festivalRepository.save(festival);
+            }
+        }
+    }
+
+    // 스케줄러를 사용해 지정 시간이 되면 데이터를 불러오도록 설정
+    @Scheduled(cron = "0 1 0 * * *") // 매일 0시 1분 실행
+    public void insertDataDetailScheduler() throws IOException {
+        List<Festival> festivalList = festivalRepository.findAll(Sort.by(Sort.Direction.DESC, "festivalId"));
+
+        for (int i = 0; i < Math.min(festivalList.size(), 10); i++) {
+            Festival festival = festivalList.get(i);
+
+            if(festival.getDetailFestival().getEventstartdate() != null) continue;
+
+            StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B551011/KorService1/detailIntro1");
+            urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + serviceKey);
+            urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("100", "UTF-8"));
+            urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8"));
+            urlBuilder.append("&" + URLEncoder.encode("MobileOS", "UTF-8") + "=" + URLEncoder.encode("ETC", "UTF-8"));
+            urlBuilder.append("&" + URLEncoder.encode("MobileApp", "UTF-8") + "=" + URLEncoder.encode("AppTest", "UTF-8"));
+            urlBuilder.append("&" + URLEncoder.encode("_type", "UTF-8") + "=" + URLEncoder.encode("json", "UTF-8"));
+            urlBuilder.append("&" + URLEncoder.encode("contentId", "UTF-8") + "=" + URLEncoder.encode(festival.getFestivalId().toString(), "UTF-8"));
+            urlBuilder.append("&" + URLEncoder.encode("contentTypeId", "UTF-8") + "=" + URLEncoder.encode("15", "UTF-8"));
+
+            URL url = new URL(urlBuilder.toString());
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-type", "application/json");
+
+            System.out.println("Response code: " + conn.getResponseCode());
+            BufferedReader rd;
+            if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+                rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            } else {
+                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            }
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                sb.append(line);
+            }
+            rd.close();
+            conn.disconnect();
+            String data = sb.toString();
+
+            try {
+                JSONObject jObject = new JSONObject(data);
+                JSONObject response = jObject.getJSONObject("response");
+                JSONObject body = response.getJSONObject("body");
+                JSONObject items = body.getJSONObject("items");
+                JSONArray jArray = items.getJSONArray("item");
+
+
+                JSONObject obj = jArray.getJSONObject(0);
+
+                String sponsor1 = obj.get("sponsor1").toString();
+                String sponsor1tel = obj.get("sponsor1tel").toString();
+                String sponsor2 = obj.get("sponsor2").toString();
+                String eventstartdate = obj.get("eventstartdate").toString();
+                String eventenddate = obj.get("eventenddate").toString();
+                String playtime = obj.get("playtime").toString();
+                String eventplace = obj.get("eventplace").toString();
+                String usetimefestival = obj.get("usetimefestival").toString();
+                String eventhomepage = obj.get("eventhomepage").toString();
+
+                festival.updateDetailFestival(new DetailFestival(sponsor1, sponsor1tel, sponsor2, eventstartdate, eventenddate, eventhomepage, playtime, eventplace, usetimefestival));
+
+                festivalRepository.save(festival);
             }catch (JSONException e) {
                 System.err.println("Failed to parse JSON data: " + e.getMessage());
                 continue;
@@ -226,4 +315,70 @@ public class FestivalApiService {
         }
     }
 
+    // 스케줄러를 사용해 지정 시간이 되면 데이터를 불러오도록 설정
+    @Scheduled(cron = "0 2 0 * * *") // 매일 0시 2분 실행
+    public void insertDataOverViewScheduler() throws IOException {
+        List<Festival> festivalList = festivalRepository.findAll(Sort.by(Sort.Direction.DESC, "festivalId"));
+
+
+        for(Festival festival : festivalList){
+
+            if(festival.getDetailFestival().getOverview() != null) continue;
+
+            StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B551011/KorService1/detailCommon1");
+            urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + serviceKey);
+            urlBuilder.append("&" + URLEncoder.encode("MobileOS", "UTF-8") + "=" + URLEncoder.encode("ETC", "UTF-8"));
+            urlBuilder.append("&" + URLEncoder.encode("MobileApp", "UTF-8") + "=" + URLEncoder.encode("AppTest", "UTF-8"));
+            urlBuilder.append("&" + URLEncoder.encode("_type", "UTF-8") + "=" + URLEncoder.encode("json", "UTF-8"));
+            urlBuilder.append("&" + URLEncoder.encode("contentId", "UTF-8") + "=" + URLEncoder.encode(festival.getFestivalId().toString(), "UTF-8"));
+            urlBuilder.append("&" + URLEncoder.encode("contentTypeId", "UTF-8") + "=" + URLEncoder.encode("15", "UTF-8"));
+            urlBuilder.append("&" + URLEncoder.encode("overviewYN", "UTF-8") + "=" + URLEncoder.encode("Y", "UTF-8"));
+
+            URL url = new URL(urlBuilder.toString());
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-type", "application/json");
+
+            System.out.println("Response code: " + conn.getResponseCode());
+            BufferedReader rd;
+            if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+                rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            } else {
+                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            }
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                sb.append(line);
+            }
+            rd.close();
+            conn.disconnect();
+            String data = sb.toString();
+
+            try {
+                JSONObject jObject = new JSONObject(data);
+                JSONObject response = jObject.getJSONObject("response");
+                JSONObject body = response.getJSONObject("body");
+                JSONObject items = body.getJSONObject("items");
+                JSONArray jArray = items.getJSONArray("item");
+
+
+                JSONObject obj = jArray.getJSONObject(0);
+                String overview = obj.optString("overview", ""); // null일 경우 ""로 반환됨
+                String category = determineCategory(overview, keywordCategoryMap);
+
+                if(festival.getDetailFestival().getEventstartdate() != null &&  festival.getDetailFestival().getEventenddate() != null){
+                    festival.getDetailFestival().updateStatus();
+                }
+
+                festival.getDetailFestival().updateCategoryAndOverView(category, overview);
+
+                festivalRepository.save(festival);
+
+            }catch (Exception e) {
+                System.err.println("Failed to parse JSON data: " + e.getMessage());
+                continue;
+            }
+        }
+    }
 }
