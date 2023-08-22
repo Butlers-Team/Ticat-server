74543fbe3af84ac1e30545d1635c6b7f70e9ccd8
jy3026 <jy_3026@naver.com>
2023-08-22 21:33:01 +0900

    Fix: 지도페이지 축제반경 10km -> 30km 변경

--- src/main/java/Butlers/Ticat/festival/service/FestivalService.java
+++ src/main/java/Butlers/Ticat/festival/service/FestivalService.java
@@ -230,9 +230,9 @@ public class FestivalService {
         }
 
         if (categories != null && !categories.isEmpty()) {
-            return festivalRepository.findFestivalsWithinDistanceAndCategoryInAndDetailFestivalStatusIn(latitude,longitude,1000.0,categories,status ,PageRequest.of(page - 1, size, sort));
+            return festivalRepository.findFestivalsWithinDistanceAndCategoryInAndDetailFestivalStatusIn(latitude,longitude,3000.0,categories,status ,PageRequest.of(page - 1, size, sort));
         } else {
-            return festivalRepository.findFestivalsWithinDistanceAndDetailFestivalStatusIn(latitude,longitude,1000.0,status,PageRequest.of(page - 1, size, sort));
+            return festivalRepository.findFestivalsWithinDistanceAndDetailFestivalStatusIn(latitude,longitude,3000.0,status,PageRequest.of(page - 1, size, sort));
         }
     }
 
